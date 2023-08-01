845ef255efff4132b186a5857952074589dd4dc7
ShirvanyanKaren <kshirvanyan2000@gmai.com>
2023-08-01 16:07:20 -0700

    added port

--- routes/notes.js
+++ routes/notes.js
@@ -5,8 +5,13 @@ const {v4: uuidv4 } = require('uuid');
 // const yes = require('../db/db.json')
 
 notes.get('/', (req, res) => {
+    
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-});
+})
+// notes.get("/", (req, res) =>
+//   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+// );
+
 
 notes.post('/', (req, res) => {
     console.log(`${req.method} request received to add a note`);
